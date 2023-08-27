ab58af5e049d5dbf511de1f51924993ae8520930
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-26 21:31:48 -0400

    feat: recursive link crawler working
    also added a counter to show the total number of links into the `pages`
    object

--- crawl.js
+++ crawl.js
@@ -16,7 +16,7 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
   // returns non-normalized URLs from page
   const dom = new JSDOM(htmlBody);
   const aTags = dom.window.document.querySelectorAll('a');
-  const foundLinks = new Set();
+  const foundLinks = [];
 
   for (const tag of aTags) {
     let fullURL;
@@ -25,25 +25,75 @@ const getURLsFromHTML = (htmlBody, baseURL) => {
     } else {
       fullURL = tag.href;
     }
-    foundLinks.add(fullURL);
+    foundLinks.push(fullURL);
   }
-  return Array.from(foundLinks);
+  return foundLinks;
 };
 
-const crawlPage = async (baseURL) => {
+const isSameDomain = (baseURL, currentURL) => {
+  return new URL(baseURL).host === new URL(currentURL).host;
+};
+
+const initPageCounter = (pages) => {
+  if (!('total_links' in pages)) {
+    pages['total_links'] = 0;
+  }
+};
+
+const crawlPage = async (baseURL, currentURL = baseURL, pages = {}) => {
+  // check if currentUrl is on same domain as baseURL, if not - return
+  // current `pages`.
   try {
-    const res = await fetch(baseURL);
-    if (res.status !== 200) {
-      throw new Error(`Received response status code ${res.status}`);
+    baseURL = new URL(baseURL);
+    currentURL = new URL(currentURL);
+
+    initPageCounter(pages);
+
+    if (!isSameDomain(baseURL, currentURL)) {
+      return pages;
     }
+    // get normalized version of the currentURL
+    const normalizedCurrentURL = normalizeURL(currentURL);
+
+    // if `pages` already has entry for normalized version, increment count and
+    // return the current `pages`
+    if (pages[normalizedCurrentURL] ?? false) {
+      pages[normalizedCurrentURL] += 1;
+      return pages;
+    }
+
+    // otherwise add entry to `pages` for normalized version of currentURL, set
+    // count to 1 as long as the current url isn't the base url. Set to 0 if it
+    // is the base URL
+    if (normalizedCurrentURL === baseURL.toString()) {
+      pages[normalizedCurrentURL] = 0;
+    } else {
+      pages[normalizedCurrentURL] = 1;
+    }
+
+    const res = await fetch(currentURL);
+    console.log(`Crawling: ${currentURL}`);
+
+    if (res.status !== 200) {
+      throw new Error(`Received Status Code: ${res.status}`);
+    }
+
     const contentType = res.headers.get('Content-Type');
-    if (!contentType || !contentType.includes('text/html')) {
+    if (!contentType.startsWith('text/html')) {
       throw new Error(`Received unwanted Content-Type: ${contentType}`);
     }
-    const data = await res.text();
-    console.log(data);
+    const html = await res.text();
+    const foundLinks = getURLsFromHTML(html, baseURL);
+
+    for (const link of foundLinks) {
+      await crawlPage(baseURL, link, pages);
+    }
+    pages['total_links'] += parseInt(foundLinks.length);
+    // return updated `pages` object
+    return pages;
   } catch (error) {
-    console.error('Error: ', error);
+    console.error(`An error occurred: ${error.message}`);
+    return pages;
   }
 };
 
--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 const { crawlPage } = require('./crawl.js');
+const { URL } = require('url');
 
-const main = () => {
+const main = async () => {
   const args = process.argv.slice(2);
   if (args.length < 1 || args.length > 1) {
     throw new Error('expected one argument, received not enough or too many.');
   }
+  const baseURL = new URL(args[0]);
 
-  console.log(`Starting crawl on ${args[0]}`);
-  crawlPage(args[0]);
+  console.log(`Starting crawl on ${baseURL}`);
+  const links = await crawlPage(baseURL);
+  console.log(links);
 };
 
 main();
