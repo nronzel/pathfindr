4ae44ae6bb4e1b5a84579b4327d3d68240b64cbb
nronzel <86695181+nronzel@users.noreply.github.com>
2023-08-27 11:22:49 -0400

    feat: more tests for object sorting

--- report.test.js
+++ report.test.js
@@ -1,4 +1,4 @@
-const { sortPages } = require('./report.js');
+const { sortObject } = require('./report.js');
 const { expect, test } = require('@jest/globals');
 
 describe('Test sorting of pages object', () => {
@@ -23,7 +23,31 @@ describe('Test sorting of pages object', () => {
       a: 1,
     };
 
-    const sorted = sortPages(testObj);
+    const sorted = sortObject(testObj);
     expect(sorted).toEqual(expected);
+  });
+
+  test('sorts empty object', () => {
+    const input = {};
+    const expected = {};
+    expect(sortObject(input)).toEqual(expected);
+  });
+
+  test('sorts object with one key-val pair', () => {
+    const input = { a: 1 };
+    const expected = { a: 1 };
+    expect(sortObject(input)).toEqual(expected);
+  });
+
+  test('sorts object with identical values', () => {
+    const input = { a: 2, b: 2, c: 1 };
+    const expected = { a: 2, b: 2, c: 1 };
+    expect(sortObject(input)).toEqual(expected);
+  });
+
+  test('sorts object with non-numeric values', () => {
+    const input = { a: 'z', b: 'a', c: 'm' };
+    const expected = { a: 'z', c: 'm', b: 'a' };
+    expect(sortObject(input)).toEqual(expected);
   });
 });
